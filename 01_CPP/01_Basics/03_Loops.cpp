d0140415c2d4603ddb436b34f359f2e43b9d1b20
Binod Dey <biniddey06@gmail.com>
2026-07-19 12:07:37 +0530

    C++ Loops

---
+++ 01_CPP/01_Basics/03_Loops.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //print numbers from 1 to 5 using FOR Loop
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    //--------------------------------------------------------
+
+    //print numbers from 1 to 5 using WHILE Loop
+    int j = 1;
+    while (j <= 5) {
+        cout << j << " ";
+        j++;
+    }
+    cout << endl;
+
+    return 0;
+}
