d00a805907d82a4d8a55ff6923dc40152d54baf4
Binod Dey <biniddey06@gmail.com>
2026-07-19 12:06:16 +0530

    Basic c++ code

---
+++ 01_CPP/01_Basics/01_BasicCode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+
+    int x; //variable creation
+    cout << "Enter a number: ";
+    cin >> x;// This line takes input from the user and stores it in the variable x.
+}
